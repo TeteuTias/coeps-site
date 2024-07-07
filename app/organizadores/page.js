f1616c748981f49380219722ce71c2eae5777d44
TeteuTias <46979510+TeteuTias@users.noreply.github.com>
2024-07-07 13:36:44 -0300

    Criação Inicial de Página "Organizadores"
    criando a pagina de organizadores. Fiz aprenas o primóridio, já que preciso de db funcionando para que ele puxe automaticamente todas as fotos.
    O header ainda nao esta responsivo, assim, já que já começei a fazer essa página, vou deixa-la pronta (tirando o que nao da pra fazer que falei acima) e
    depois vou arrumar a responsividade com state.
    Ainda precisa tornar a página funcional e também deixa-la responsiva

---
+++ app/organizadores/page.js
@@ -0,0 +1,243 @@
+import Image from "next/image";
+//
+//
+export default () =>{
+    const dict_fotos = {
+        "TODAS":"só para lembrar que TODAS possui uma lógica especial.",
+        "GERAL": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+        ],        
+        "ALIMENTOS": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+        ],          
+        "APOIO":[ 
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+        ],          
+        "CAPACITAÇÃO": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+        ],          
+        "CERTIFICADOS":[ 
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            
+        ],          
+        "CIENTÍFICA": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+        ],          
+        "EVENTOS": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+
+        ],          
+        "FINANÇAS": [            
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            
+        ],          
+        "INFRAESTRUTURA": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            
+        ],          
+        "INTERCURSOS": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+        ],      
+        "LIGAS": [
+
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+        ],     
+        "MARKETING": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+        ],          
+        "PROGRAMAÇÃO": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+        ],          
+        "PRODUTOS": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            
+        ],          
+        "SECRETARIADO": [
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+            {"Jotinha":"https://coeps.com.br/wp-content/uploads/2023/10/5-1.png"},
+
+        ],               
+    }
+    const lista_fotos = [ // so fiz assim para demostração, por enquanto nao tem separação por coordenadoria
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+        "https://coeps.com.br/wp-content/uploads/2023/10/5-1.png",
+
+    ]
+    var organizacao_selecionada = "TODAS"
+    return (
+        <div className="">
+            <div className="bg-[url(Site.jpg)] flex content-center justify-center font-semibold text-[30px] p-16  bg-center bg-cover">
+                <h1>Comissão Organizadora</h1>
+            </div>
+            <div className="flex content-center justify-center pb-10">
+                <div className=" w-2/3 space-y-6">
+                    <div className="flex content-center justify-center">
+                        <h1 className=" text-gray-700">
+                            O <span className="text-gray-800 font-bold">V COEPS</span>, desenhado e objetivado pelo DADG, é inteiramente organizado pelos alunos do Centro Universitário IMEPAC Araguari. 
+                            Tradicionalmente, sempre foi realizado pelos alunos da Medicina, porém essa edição contará com alunos de outros cursos da saúde, 
+                            como psicologia, enfermagem, educação física, fisioterapia e outros.
+                        </h1>
+                    </div>
+                    <div className="flex content-center justify-center">
+                        <h1 className=" text-gray-700">
+                            Conheça nossa comissão organizadora abaixo!
+                        </h1>
+                    </div>
+                    <div className="flex flex-col justify-center items-center content-center space-y-6">
+                        <div className="flex flex-wrap gap-4 justify-center w-[80%] ">
+                            {
+                            Object.keys(dict_fotos).map((value, index) => {
+                                return (
+                                    <div key={index} className={`${index == organizacao_selecionada || "TODAS" == value?"bg-gray-800 text-white font-bold":"bg-white text-gray-700"}  p-1 px-2 shadow-md text-center cursor-pointer `}>
+                                        <p className="text-sm">{value}</p>
+                                    </div>
+                                )
+                            })
+                            }
+                        </div>
+                        <div className="grid grid-rows-4 grid-flow-col gap-4">
+                        {
+                                lista_fotos.map((value,index)=>{
+                                    return (
+                                    <div key={index} className="relative w-[100%]">
+                                        <Image
+                                            srcset={value}
+                                            alt="Descrição da imagem"
+                                            layout="fit"
+                                            objectFit="cover"
+                                            className=""
+                                        />
+                                    </div>
+                                )
+                            })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+//
+//
