e31c9b6ac6a2fb61ed9b91fd5ba57c2746ce495c
TeteuTias <46979510+TeteuTias@users.noreply.github.com>
2024-07-07 02:00:29 -0300

    Alterações simples
    Alterando o footer e Header para layout.js

--- app/page.js
+++ app/page.js
@@ -4,8 +4,6 @@ export default function Home() {
   return (
     <main className="w-screen">
       <div className="bg-[url(Site.jpg)] bg-center bg-cover pb-10">
-        <Header01 />
-        <Header02 />
         <Letreiro />
         <Contador />
       </div>
@@ -18,69 +16,11 @@ export default function Home() {
       <div className="bg-white relative" data-negative="false">
         <BannerDIV4 />
       </div>
-      <Footer />
+      
     </main>
   );
 }
 // Premeiro DIV
-
-function Header01() {
-  return(
-      <div className="bg-[#3E4095] flex p-2 flex-col space-y-10 lg:flex-row lg:items-center lg:justify-center lg:content-center lg:space-x-10 lg:space-y-0 ">
-        <div>
-          <h1>vcoeps.dadg@gmail.com</h1>
-        </div>
-        <div>
-          <h1>(62) 98330-6426</h1>
-        </div>
-        <div>
-          <h1>Área do Congressista</h1>
-        </div>
-      </div>
-  )
-}
-function Header02() {
-  return(
-      <div className="top-0 sticky bg-green-200/10  p-4 flex flex-col space-y-10 lg:flex-row lg:border-[1px] lg:items-center lg:justify-center lg:content-center lg:space-x-10 lg:space-y-0 ">
-        <div className="hover:text-red-800 ease-in duration-150 cursor-pointer">
-          <Image       
-            src="/Logo01.png"
-            width={150}
-            height={150}
-            alt="Picture of the author"
-          />
-        </div>
-        <div className="hover:text-red-800 ease-in duration-150 cursor-pointer">
-          <h1>Inicio</h1>
-        </div>
-        <div className=" hover:text-red-800 ease-in duration-150 cursor-pointer">
-          <h1>O Evento</h1>
-        </div>
-        <div className=" hover:text-red-800 ease-in duration-150 cursor-pointer">
-
-          <h1>Palestrantes</h1>
-        </div>
-        <div className=" hover:text-red-800 ease-in duration-150 cursor-pointer">
-          <h1>Programação</h1>
-        </div>
-        <div className=" hover:text-red-800 ease-in duration-150 cursor-pointer">
-          <h1>Inscrições</h1>
-        </div>
-        <div className=" hover:text-red-800 ease-in duration-150 cursor-pointer">
-          <h1>Trabalhos</h1>
-        </div>
-        <div className=" hover:text-red-800 ease-in duration-150 cursor-pointer">
-          <h1>Anais</h1>
-        </div>
-        <div className=" hover:text-red-800 ease-in duration-150 cursor-pointer">
-          <h1>Contato</h1>
-        </div>
-        <div className=" hover:text-red-800 ease-in duration-150 cursor-pointer">
-          <h1>Inscreva-se</h1>
-        </div>
-      </div>
-  )
-}
 function Letreiro() {
  return (
   <div className="flex flex-col space-y-8 justify-center items-center content-center p-10">
@@ -255,32 +195,6 @@ function BannerDIV4(){
 }
 //
 // Footer
-function Footer() {
-  return (
-    <div className="flex justify-center items-center content-center mt-10">
-      <div className="flex flex-col justify-center items-center content-center">
-        <h1 className="font-bold text-[#3e4095] text-[20px]">Realização</h1>
-        <div className="flex flex-row space-x-5">
-          <div>
-            <Image       
-              src="/LogoDADG.png"
-              width={150}
-              height={150}
-              alt="Picture of the author"
-            />
-          </div>
-          <div>
-            <Image       
-              src="/LogoImepac.png"
-              width={150}
-              height={150}
-              alt="Picture of the author"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+
 
 //<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg>
