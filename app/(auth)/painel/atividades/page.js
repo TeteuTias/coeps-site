7967eda5d2a635661f05159d813c25bc980c39e8
TeteuTias <46979510+TeteuTias@users.noreply.github.com>
2024-07-22 20:50:05 -0300

    upgrade /painel/atividades
    Agora ele já identifica quais podem e quais não podem ter inscrições. Baixei uma nova chamada luxon para lidar com as datas. Fiz tambem uma nova rota de API para GET

--- app/(auth)/painel/atividades/page.js
+++ app/(auth)/painel/atividades/page.js
@@ -1,13 +1,43 @@
 'use client'
-
+import { useEffect, useState } from "react"
 import HeaderPainel from "@/app/components/HeaderPainel"
+import WarningModal from "@/app/components/WarningModal"
+import { DateTime } from "luxon"
 //
 //
 //
 export default function Minicursos() {
-    const apagar = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9, 0, 0,]
+    const [loadingData, setLoadingData] = useState(1)
+    const [data, setData] = useState([])
+    //
+    //
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+
+                const response = await fetch('/api/get/atividades')
+                const jsonResponse = await response.json()
+                if (!response.ok) {
+                    console.log(jsonResponse)
+                    return 0
+                }
+                setData(jsonResponse)
+            }
+            catch (error) {
+                console.log(error)
+            }
+            finally {
+                setLoadingData(0)
+            }
+        }
+        if (loadingData) {
+            fetchData()
+        }
+    }, [loadingData])
+
     return (
         <div className="bg-[#3E4095] min-h-screen">
+            <button className="bg-black text-white p-4" onClick={() => { console.log(data) }}>click</button>
             <div className=" w-full text-center flex items-center justify-center">
                 <div className="w-[90%] lg:w-[60%] text-justify p-5">
                     <h1 className="break-words text-center font-extrabold text-white text-[22px] lg:text-[35px]">Atividades</h1>
@@ -17,7 +47,8 @@ export default function Minicursos() {
                 <div className="w-[95%] md:w-[50%]">
                     <h1 className="break-words text-start font-bold text-black text-[22px] lg:text-[18px]">O QUE TEMOS AQUI</h1>
                     <h1 className="text-justify">
-                        Aqui, você pode se inscrever em <span className="bg-yellow-300 px-1 font-bold">atividades complementares</span>. É obrigatório que cada participante escolha <span className="font-bold">três</span> dessas atividades. Lembre-se de que cada uma possui um número máximo de participantes, portanto, programe-se para se inscrever a tempo! Não se preocupe, todas as informações necessárias estão disponíveis aqui. Após a inscrição, a atividade será adicionada automaticamente à <span className="bg-yellow-300 px-1 font-bold">Minha Programação</span>.
+                        Aqui, você pode se inscrever em <span className="bg-yellow-300 px-1 font-bold">atividades complementares</span>. É obrigatório que cada participante escolha <span className="font-bold">três</span> dessas atividades. Lembre-se de que cada uma possui um número máximo de participantes, portanto, programe-se para se inscrever a tempo! Não se preocupe, todas as informações necessárias estão disponíveis aqui. Após a inscrição, a atividade será adicionada automaticamente à <span className="bg-yellow-300 px-1 font-bold">Minha Programação</span>. Você consegue ver mais detalhes
+                        sobre as atividades <span className="bg-yellow-300 px-1 cursor-pointer font-bold">clicando aqui</span>.
 
                     </h1>
                 </div>
@@ -27,7 +58,7 @@ export default function Minicursos() {
                     <h1 className="break-words text-start font-bold text-white text-[22px] lg:text-[18px]">PRECISO PAGAR?</h1>
                     <h1 className="text-justify">
                         Grande parte das atividades é <span className="bg-yellow-300 text-gray-800 font-bold px-1">gratuita</span>. Entretanto, algumas podem ter cobranças simbólicas para viabilizar
-                        o evento.  Atividades pagas estão marcadas com <span className="bg-yellow-300 text-gray-800 font-bold px-1">PAGO</span>.
+                        o evento.  Atividades pagas estão marcadas com <span className="bg-yellow-300 text-gray-800 font-bold px-1 font-emoji">💲</span>.
                     </h1>
                 </div>
             </div>
@@ -47,27 +78,52 @@ export default function Minicursos() {
             <div className="flex content-center justify-center w-full text-gray-700 text-clip py-16 ">
                 <div className="flex flex-col w-[95%] md:w-[70%]">
                     <div className="text-center">
+                        <h1 className="break-words font-bold text-white text-[22px] lg:text-[18px]">
+                            {
+                                loadingData ? 'CARREGANDO ATIVIDADES' : ""
+                            }
+                            {
+                                !loadingData && data.length == 0 ? "AINDA NÃO HÁ ATIVIDADES DISPONÍVEIS" : ""
+                            }
+                        </h1>
                         <h1 className="break-words font-bold text-white text-[22px] lg:text-[18px]">TYPE - COLOCAR O TYPE PARA FICAR MAIS FÁCIL |CARREGANDO|MSG QUANDO CARREGADO| MENSAGEM QUANDO NÃO TEM NADA</h1>
                     </div>
+
                     <div className="flex items-center content-center justify-center">
-                        <div className="w-[95%] md:w-[90%] grid grid-cols-1 gap-x-10 gap-y-10 lg:grid-cols-3 lg:gap-2 lg:gap-x-10 lg:gap-y-10 p-4">
-                            {
-                                apagar.map((index) => {
-                                    return (
-                                        <div key={index}>
-                                            <BannerAtividade />
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
+                        {
+                            !loadingData && data.length > 0 &&
+                            <div className="w-[90%] sm:w-[65%] 2xl:w-[90%] grid grid-cols-1 gap-x-10 gap-y-10 p-4 2xl:grid-cols-3 2xl:gap-2 2xl:gap-x-10 2xl:gap-y-10 lg:grid-cols-2 lg:gap-2 lg:gap-x-10 lg:gap-y-10 ">
+                                {
+                                    data.map((value, index) => {
+
+                                        return (
+                                            <div key={value._id}>
+                                                <BannerAtividade activity={value} />
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
         </div>
     )
 }
+const isDateEqualOrAfterToday = (inputDate) => {
+    // Define a data atual no fuso horário -03:00 (America/Sao_Paulo)
+    const currentDate = DateTime.now().setZone('America/Sao_Paulo');
 
+    // Converte a string de data fornecida para um objeto DateTime no mesmo fuso horário
+    const inputDateTime = DateTime.fromISO(inputDate, { zone: 'America/Sao_Paulo' });
+
+    // Compara as duas datas
+    if (currentDate >= inputDateTime) {
+        return "INSCREVER"
+    }
+    return 'FECHADO'
+}
 function generateHexColor() {
     const targetColor = [0x3e, 0x40, 0x95]; // RGB values of #3e4095
     const threshold = 100; // Threshold to avoid colors too close to #3e4095
@@ -108,28 +164,49 @@ function generateHexColor() {
     return color;
 }
 
-const BannerAtividade = () => {
+const BannerAtividade = ({ activity }) => {
+    /*
+    name
+    emoji
+    description
+    isOpen
+    dateOpen
+    isFree
+    */
+
+    const buttonText = isDateEqualOrAfterToday(activity.dateOpen)
     var color = generateHexColor()
+
     return (
-        <div className="relative">
-            <div className="absolute  w-fit p-2" style={{ 'backgroundColor': color }}>
-                <h1 className="font-emoji">💲</h1>
-            </div>
-            <div className=" bg-white shadow-2xl">
+        <div className="relative bg-white min-h-[600px] max-h-[600px] shadow-2xl">
+            {
+                !activity.isFree ?
+                    <div className="absolute  w-fit p-2" style={{ 'backgroundColor': color }}>
+                        <h1 className="font-emoji">💲</h1>
+                    </div> : ""
+            }
+            <div className="   ">
                 <div className={`p-[3px]`} style={{ 'backgroundColor': color }} />
-                <div className="p-5 space-y-5">
+                <div className="p-5 space-y-5 h-[520px] overflow-auto">
                     <div className="text-center">
-                        <h1 className="text-[100px] font-emoji">👨‍⚕️</h1>
+                        <h1 className="text-[100px] font-emoji">{activity.emoji}</h1>
                     </div>
                     <div>
-                        <h1 className="font-bold text-center">{'Terapia Hormonal de Reposição de Testosterona'.toLocaleUpperCase()}</h1>
+                        <h1 className="font-bold text-center">{activity.name.toLocaleUpperCase()}</h1>
                     </div>
                     <div>
-                        <h1 className="font-thin text-center">Amanhecer no campo é um espetáculo para os sentidos. O canto dos pássaros saúda o novo dia enquanto a brisa suave espalha o aroma fresco da terra úmida. Os primeiros raios de sol tocam suavemente as folhas das árvores, criando um mosaico de luz e sombra no chão. As flores começam a se abrir, revelando suas cores vibrantes e atraindo abelhas e borboletas. No horizonte, as montanhas se destacam contra o céu azul, prometendo aventuras para aqueles que se atrevem a explorar. É um momento de paz e renovação, onde a natureza revela sua beleza e serenidade em cada detalhe.</h1>
+                        <h1 className="font-thin text-center">
+                            {activity.description}
+                        </h1>
                     </div>
-                    <div className="flex justify-center">
-                        <button className="p-4 font-bold text-white" style={{ 'backgroundColor': color }}>INSCREVER</button> {/* INSCREVER|JÁ INSCRITO|FECHADO  */}
-                    </div>
+                    <h1>{ }</h1>
+                </div>
+                <div className="flex justify-center ">
+                    <button className="p-4 font-bold text-white" style={{ 'backgroundColor': color }}>
+                        {
+                            activity.isOpen ? buttonText : "FECHADO"
+                        }
+                    </button> {/* INSCREVER|JÁ INSCRITO|FECHADO  */}
                 </div>
             </div>
         </div>
---
+++ app/api/get/atividades/route.js
@@ -0,0 +1,94 @@
+import { connectToDatabase } from '../../../lib/mongodb'
+import { NextResponse } from 'next/server';
+import { getAccessToken } from '@auth0/nextjs-auth0';
+import { execOnce } from 'next/dist/shared/lib/utils';
+import { ObjectId } from 'mongodb';
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
+//
+//
+// Exemplo de return:
+// {"data":{"isPos_registration":0,"informacoes_usuario":{"nome:":"","email":"mateus2.0@icloud.com","data_criacao":"2024-07-08T22:48:41.110Z"}}}
+// Exemplo de return erro:
+// 
+export const GET = withApiAuthRequired(async function GET(request, { params }) {
+    try {
+        // Verificando se está logado
+        // Puxando configs
+        const { db } = await connectToDatabase();
+        const colecao = "minicursos"
+        const result = await db.collection(colecao).find(
+            {},
+        ).toArray()
+        return NextResponse.json(result, { status: 200 });
+
+    }
+    catch (error) {
+        //console.log(error)
+        return NextResponse.json({ "error": error }, { status: 500 })
+    }
+})
+/*             
+[
+  {
+    "_id": "66934efb29c777824717507b",
+    "name": "Terapia Hormonal de Reposição de Testosterona",
+    "description": "Palestra sobre testo e como usar",
+    "maxParticipants": 40,
+    "participantsCount": 0,
+    "participants": [
+      "6692c5208072c6037885c94d",
+      "669b0e73f89da1fa30deb6ee",
+      "669b112fa2a21905b47571a7"
+    ],
+    "isFree": 1,
+    "timeline": [
+      {
+        "_id": "66934efb29c777824717507b",
+        "name": "Palestra sobre HBP",
+        "date_init": "2024-07-18T13:00:00-03:00",
+        "date_end": "2024-07-18T15:30:00-03:00",
+        "description": "Palestra sobre HBP com o Dr. Humberto de Morais. Vamos abrodar o tema profundamente...",
+        "speakers": [],
+        "presence_list": [],
+        "local_description": "Não se atrase",
+        "local": "Pegaremos um ônibus em frente a IMEPAC.",
+        "_idPattern": "66934efb29c777824717507b"
+      }
+    ],
+    "isOpen": 1,
+    "dateOpen": "2024-07-18T13:00:00-03:00",
+    "type": "teste3",
+    "organization_name": "UROLIGA"
+  },
+  {
+    "_id": "669c743fc07af596f70fb7ac",
+    "name": "Terapia Hormonal de Reposição de Testosterona",
+    "description": "Palestra sobre testo e como usar",
+    "maxParticipants": 40,
+    "participantsCount": 0,
+    "participants": [
+      "6692c5208072c6037885c94d",
+      "669b112fa2a21905b47571a7"
+    ],
+    "isFree": 1,
+    "timeline": [
+      {
+        "_id": "66934efb29c777824717507b",
+        "name": "Início",
+        "date_init": "2024-07-18T13:00:00-03:00",
+        "date_end": "2024-07-18T15:30:00-03:00",
+        "description": "Curso Teórico Prático",
+        "speakers": [],
+        "presence_list": [],
+        "local_description": "Ao lado do ambulatório",
+        "local": "Centro de Simulação Realística",
+        "_idPattern": "669c743fc07af596f70fb7ac"
+      }
+    ],
+    "isOpen": 1,
+    "dateOpen": "2024-07-18T13:00:00-03:00",
+    "type": "teste4",
+    "organization_name": "UROLIGA"
+  }
+]
+*/
