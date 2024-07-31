20ef7614eb5482d0aa0238acc93ebaf94f18cf96
Mateus <mateus2.0@icloud.com>
2024-07-31 19:55:56 -0300

    Update \unauth\Trabalhos
    Agora, é possível decidir diretamente no mongoDb se está aberto ou não o processo de trabalhos. Ambos possuem textos pré-definidos.

--- app/(unauth)/trabalhos/page.js
+++ app/(unauth)/trabalhos/page.js
@@ -47,12 +47,24 @@ export default function Trabalhos() {
                 </div>
             </div>
             <div className="flex flex-col justify-center content-center items-center relative pt-10 pb-20">
-                <div className="flex flex-col space-y-10 w-[90%] lg:space-y-0 lg:space-x-10 lg:flex-row justify-center content-center items-center">
-                    <CardDatas data={config?.data_limite_submissao ? new Date(config.data_limite_submissao).toLocaleDateString().slice(0, 5) : ""} texto="Limite de Submissão" />
-                    <CardDatas data={config?.trabalhos_por_usuario ? String(config?.trabalhos_por_usuario).padStart(2, '0') : ""} texto="Trabalhos por autor" />
-                    <CardDatas data={config?.autores_por_trabalho ? String(config?.autores_por_trabalho).padStart(2, '0') : ""} texto="Autores por Trabalho" />
-                    <CardDatas data={config?.data_publicacao_resultados ? new Date(config?.data_publicacao_resultados).toLocaleDateString().slice(0, 5) : ""} texto="Publicação de resultados" />
-                </div>
+                {
+                    loading ?
+                        <h1 className="font-semibold text-[#3E4095] text-[25px] lg:text-[30px]"><span></span>CARREGANDO</h1>
+                        : config?.isOpen ? (
+                            <div className="flex flex-col space-y-10 w-[90%] lg:space-y-0 lg:space-x-10 lg:flex-row justify-center content-center items-center">
+                                <CardDatas data={config?.data_limite_submissao ? new Date(config.data_limite_submissao).toLocaleDateString().slice(0, 5) : ""} texto="Limite de Submissão" />
+                                <CardDatas data={config?.trabalhos_por_usuario ? String(config?.trabalhos_por_usuario).padStart(2, '0') : ""} texto="Trabalhos por autor" />
+                                <CardDatas data={config?.autores_por_trabalho ? String(config?.autores_por_trabalho).padStart(2, '0') : ""} texto="Autores por Trabalho" />
+                                <CardDatas data={config?.data_publicacao_resultados ? new Date(config?.data_publicacao_resultados).toLocaleDateString().slice(0, 5) : ""} texto="Publicação de resultados" />
+                            </div>
+                        ) :
+                            <div className="flex flex-col space-y-10 w-[90%] justify-center content-center items-center">
+                                <h1 className="text-[#54595f]">
+                                    Ainda não definimos a data para a abertura das submissões de trabalhos. No entanto, não se preocupe! Acompanhe nosso site
+                                    e nossas redes sociais para receber as atualizações mais recentes e ficar por dentro das próximas datas.
+                                </h1>
+                            </div>
+                }
                 <div className=" w-[90%]">
                     <div className="pt-10">
                         <h1 className="font-semibold text-slate-950 text-[30px] lg:text-[35px]"><span></span>Publicações</h1>
@@ -77,28 +89,41 @@ export default function Trabalhos() {
                         }
                     </div>
                     <div className="pt-10">
-                        <p className="text-[#54595f] text-justify">
-                            O Diretório Acadêmico Diogo Guimarães (DADG) do curso de graduação em Medicina do Centro Universitário IMEPAC Araguari apresenta o <span className="text-gray-800 font-bold">V Congresso dos Estudantes
-                                e Profissionais de Saúde (COEPS)</span> que possui como tema “Cuidados Paliativos na formação profissional de saúde: humanização em destaque”. Com o intuito de
-                            incentivar a participação dos acadêmicos, profissionais da saúde e áreas afins em atividades de pesquisa, visando complementar a formação acadêmica e
-                            enriquecer conhecimentos, declara-se aberto o edital para a submissão de trabalhos inéditos pertinentes à área da saúde.
-                        </p>
+                        {
+                            !loading && config?.isOpen ?
+                                <p className="text-[#54595f] text-justify">
+                                    O Diretório Acadêmico Diogo Guimarães (DADG) do curso de graduação em Medicina do Centro Universitário IMEPAC Araguari apresenta o <span className="text-gray-800 font-bold">VI Congresso dos Estudantes
+                                        e Profissionais de Saúde (COEPS)</span> que possui como tema “Inovação em saúde: Conectando Ciência Moderna ao Cuidado Tradicional”. Com o intuito de
+                                    incentivar a participação dos acadêmicos, profissionais da saúde e áreas afins em atividades de pesquisa, visando complementar a formação acadêmica e
+                                    enriquecer conhecimentos, declara-se aberto o edital para a submissão de trabalhos inéditos pertinentes à área da saúde.
+                                </p> :
+                                <p className="text-[#54595f] text-justify">
+                                    O Diretório Acadêmico Diogo Guimarães (DADG) do curso de Medicina do Centro Universitário IMEPAC Araguari está preparando o <span className="text-gray-800 font-bold">VI Congresso dos Estudantes e Profissionais
+                                        de Saúde (COEPS)</span>, com o tema “Inovação em saúde: Conectando Ciência Moderna ao Cuidado Tradicional”.
+                                    Atualmente, o edital para a submissão de trabalhos inéditos ainda não está aberto. No entanto, em breve, divulgaremos mais
+                                    informações sobre como participar e submeter suas pesquisas. Fique atento ao nosso site e às nossas redes sociais para atualizações e detalhes sobre o processo de submissão.
+                                </p>
+                        }
                     </div>
-                    <div className="flex flex-row space-x-10 pt-12">
-                        <Link href={config?.link_edital ?? ""} target="_blank" prefetch={false}>
-                            <div>
-                                <button className="bg-[#3E4095] text-white p-2 px-4">
-                                    {config?.link_edital ? "VER EDITAL" : "CARREGANDO"}
-                                </button>
+                    {
+                        !loading && config?.isOpen ?
+                            <div className="flex flex-row space-x-10 pt-12">
+                                <Link href={config?.link_edital ?? ""} target="_blank" prefetch={false}>
+                                    <div>
+                                        <button className="bg-[#3E4095] text-white p-2 px-4">
+                                            {config?.link_edital ? "VER EDITAL" : "CARREGANDO"}
+                                        </button>
 
+                                    </div>
+                                </Link>
+                                <div>
+                                    <Link href="/painel/trabalhos" prefetch={false}>
+                                        <button className="bg-[#3E4095] text-white p-2 px-4">ENVIAR TRABALHO</button>
+                                    </Link>
+                                </div>
                             </div>
-                        </Link>
-                        <div>
-                            <Link href="/painel/trabalhos" prefetch={false}>
-                                <button className="bg-[#3E4095] text-white p-2 px-4">ENVIAR TRABALHO</button>
-                            </Link>
-                        </div>
-                    </div>
+                            : ""
+                    }
                 </div>
             </div >
             <div>
