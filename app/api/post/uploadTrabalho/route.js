b571ddccdc8a5e50c92ba731264915f83dc44f9c
Mateus <mateus2.0@icloud.com>
2024-10-10 20:36:10 -0300

    Resolução de bug certificados
    O post não estava configurado para receber tanto dado assim. Agora a largura da banda foi adequada.

--- app/api/post/uploadTrabalho/route.js
+++ app/api/post/uploadTrabalho/route.js
@@ -1,23 +1,22 @@
 // pages/api/upload.js
 import { GridFSBucket } from 'mongodb';
-
 import { connectToDatabase } from '@/app/lib/mongodb';
 import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
-//
+
+export const runtime = 'nodejs'; // Mude para 'nodejs' para evitar o Edge Runtime
+
 // Variáveis Globais
-const MAX_FILE_SIZE = 100 * 1024 * 1024; // 50MB
+const MAX_FILE_SIZE = 100 * 1024 * 1024; // 100MB
 
-//
-//
-//
 export const POST = withApiAuthRequired(async function POST(request) {
-
     try {
         const { user } = await getSession(request);
         const userId = user.sub.replace("auth0|", "");
-        // Verificando se ainda dá tempo de enviar formulário.
-        const { data_inicio_submissao, data_limite_submissao } = await getDatesFromDataBase()
-        const verf = await verfSubmition(data_inicio_submissao, data_limite_submissao)
+
+        // Verificando se ainda dá tempo de enviar o formulário.
+        const { data_inicio_submissao, data_limite_submissao } = await getDatesFromDataBase();
+        await verfSubmition(data_inicio_submissao, data_limite_submissao);
+
         // Pegando componentes de formulário
         const formData = await request.formData();
         const files = formData.getAll("file");
@@ -41,22 +40,21 @@ export const POST = withApiAuthRequired(async function POST(request) {
         return Response.json({ message: err.message || "Ocorreu um erro desconhecido. Recarregue a página e tente novamente. Caso o erro persista, entre em contato com a equipe COEPS."}, { status: err.status || 500 });
     }
 });
+
 // Função que verifica se a data atual ainda permite que o usuário submeta o trabalho dele.
 const verfSubmition = async (data_inicio_submissao, data_limite_submissao) => {
-    // Puxando data atual.
-    const dataAtual = new Date()
-    dataAtual.setHours(new Date().getHours() - 3)
-    //
+    const dataAtual = new Date();
+    dataAtual.setHours(dataAtual.getHours() - 3);
+
     const inicio = new Date(data_inicio_submissao);
     const limite = new Date(data_limite_submissao);
-    // Verificando se x está entre inicio e limite
-    if (dataAtual >= inicio && dataAtual <= limite) {
-        //
-    } else {
+
+    if (!(dataAtual >= inicio && dataAtual <= limite)) {
         throw { message: "Infelizmente, o prazo para a submissão de trabalhos já se encerrou.", status: 409 };
     }
-}
-// Função para buscar data_ de dp
+};
+
+// Função para buscar as datas de submissão no banco de dados
 const getDatesFromDataBase = async () => {
     const { db } = await connectToDatabase();
     const collection = 'trabalhos_config';
@@ -82,22 +80,21 @@ const validateFile = (files) => {
 
     const file = files[0];
     if (file.size > MAX_FILE_SIZE) {
-        throw { message: "O arquivo excede o limite máximo de 10MB por arquivo enviado.", status: 413 };
+        throw { message: "O arquivo excede o limite máximo de 100MB por arquivo enviado.", status: 413 };
     }
 
     return file;
 };
 
+// Função para inserir o arquivo no banco de dados
 const insertFileIntoDatabase = async (file, userId) => {
     const arrayBuffer = await file.arrayBuffer();
     const buffer = new Uint8Array(arrayBuffer);
 
     const { db } = await connectToDatabase();
 
-    // Criando um bucket do GridFS
     const bucket = new GridFSBucket(db, { bucketName: 'trabalhos' });
 
-    // Criando um stream de upload
     const uploadStream = bucket.openUploadStream(file.name, {
         metadata: {
             user_id: userId,
@@ -105,7 +102,6 @@ const insertFileIntoDatabase = async (file, userId) => {
         }
     });
 
-    // Escrevendo o buffer no GridFS
     uploadStream.end(buffer);
 
     return new Promise((resolve, reject) => {
@@ -117,23 +113,4 @@ const insertFileIntoDatabase = async (file, userId) => {
             reject(err);
         });
     });
-};
-
-
-// Função para processar e inserir o arquivo no banco de dados
-const insertFileIntoDatabaseAntigo = async (file, userId) => {
-    const arrayBuffer = await file.arrayBuffer();
-    const buffer = new Uint8Array(arrayBuffer);
-
-    const { db } = await connectToDatabase();
-    const collection = 'trabalhos';
-
-    const response = await db.collection(collection).insertOne({
-        name: file.name,
-        size: file.size,
-        user_id: userId,
-        buffer,
-    });
-
-    return response.insertedId;
 };
