c46377e02323fbecb0e6f0784819e11fa02e005e
Mateus <mateus2.0@icloud.com>
2024-10-26 01:11:07 -0300

    apenas para teste de desempenho. Pode apagar.

---
+++ app/api/teste/route.js
@@ -0,0 +1,35 @@
+// import { connectToDatabase } from '../../../lib/mongodb'
+import { connectToDatabase } from '@/app/lib/mongodb';
+
+import { NextResponse } from 'next/server';
+import { getAccessToken } from '@auth0/nextjs-auth0';
+import { execOnce } from 'next/dist/shared/lib/utils';
+import { ObjectId } from 'mongodb';
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
+//
+//
+// Exemplo de return:
+// {"data":{"isPos_registration":0,"informacoes_usuario":{"nome:":"","email":"mateus2.0@icloud.com","data_criacao":"2024-07-08T22:48:41.110Z"}}}
+// Exemplo de return erro:
+// 
+
+export const dynamic = 'force-dynamic'
+
+export async function GET(request, { params }) {
+  try {
+    // Verificando se está logado
+    // Puxando configs
+
+    const { db } = await connectToDatabase();
+    const result = await db.collection("usuarios").find(
+      {},
+
+    ).toArray()
+    return NextResponse.json({ "Olá": "Mundo!" }, { status: 200 })
+
+  }
+  catch (error) {
+    //console.log(error)
+    return NextResponse.json({ "message": error }, { status: 500 })
+  }
+}
