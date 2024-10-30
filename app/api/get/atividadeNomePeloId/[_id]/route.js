336d307a0df3249c5af73c16e182dd908f4e061c
Mateus <mateus2.0@icloud.com>
2024-10-30 08:08:58 -0300

    colocar nome minicurso na tela.

---
+++ app/api/get/atividadeNomePeloId/[_id]/route.js
@@ -0,0 +1,24 @@
+// pages/api/download.js
+import { ObjectId, GridFSBucket } from 'mongodb';
+import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
+import { connectToDatabase } from '@/app/lib/mongodb';
+import { NextResponse } from 'next/server';
+
+
+export const GET = withApiAuthRequired(async function GET(request, { params }) {
+    try {
+        const eventId = params["_id"];
+
+        const { db } = await connectToDatabase();
+        const result = await db.collection("minicursos").find(
+            { "_id": new ObjectId(eventId) },
+
+        ).toArray()
+
+        return NextResponse.json({ data: result[0].name || "ERROR" }, { status: 200 });
+
+    } catch (error) {
+        console.log(error.message);
+        return new Response(JSON.stringify({ message: error.message || "Ocorreu um erro desconhecido. Recarregue a página e tente novamente. Caso o erro persista, entre em contato com a equipe COEPS." }), { status: 500, headers: { 'Content-Type': 'application/json' } });
+    }
+});
