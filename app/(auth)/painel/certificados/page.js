b6084dd40a7c8816bcc933e0d3e3cf40be731742
Mateus <mateus2.0@icloud.com>
2024-11-03 00:37:07 -0300

    atualização certificados
    Agora é possível baixar e ver os certificados disponíveis dos usuários.

--- app/(auth)/painel/certificados/page.js
+++ app/(auth)/painel/certificados/page.js
@@ -4,10 +4,106 @@ import { useEffect, useState } from 'react';
 import CardDatas from '@/app/components/CardDatas';
 import Link from 'next/link';
 import WarningModal from '@/app/components/WarningModal';
+
+import TelaLoading from '@/app/components/TelaLoading';
 //
 //
 export default function Home() {
-    
+    const [firstLoading, setFirstLoading] = useState(true)
+    const [dataOrganizador, setDataOrganizador] = useState()
+    const [isLoadingDeleteOrSend, setIsLoadingDeleteOrSend] = useState(false)
+    const [err, setErr] = useState()
+
+    const baixarCertificadoOrganizador = async (componentId) => {
+        console.log(componentId)
+        setIsLoadingDeleteOrSend(1)
+        try {
+            const response = await fetch(`/api/get/certificadoOrganizador/${componentId}`, {
+                method: 'GET',
+            });
+
+            if (!response.ok) {
+                throw new Error('Erro ao fazer o download do arquivo');
+            }
+
+            // Extrair o nome do arquivo do cabeçalho
+            const contentDisposition = response.headers.get('Content-Disposition');
+            const filename = contentDisposition
+                ? contentDisposition.split('filename=')[1]
+                : 'downloaded_file';
+
+            // Criar um blob a partir do stream de resposta
+            const blob = await response.blob();
+
+            // Criar um link temporário para download
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = filename.replace(/"/g, ''); // Remover aspas do nome do arquivo, se houver
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url); // Limpar o URL temporário
+        } catch (error) {
+            // setIsModalError(error.message || "Ocorreu algum erro ao tentar baixar seu arquivo. Recarregue a página e tente novamente. Caso o erro persista, entre em contato com a equipe COEPS.")
+            console.error('Erro ao baixar o arquivo:', error);
+        }
+        finally {
+            // setIsModalError("Seu download foi realizado com sucesso!")
+            // setIsLoadingDeleteOrSend(0)
+        }
+    };
+
+
+    useEffect(() => {
+        // URLs das APIs
+        const urls = [
+            '/api/get/usuarioOrganizacao',
+        ];
+
+        // Função que faz as requisições
+        const fetchData = async () => {
+            try {
+
+                // Cria um array de Promises para cada URL
+                const [result1] = await Promise.all(
+                    urls.map(url =>
+                        fetch(url)
+                            .then(response => {
+                                if (!response.ok) {
+                                    throw new Error(`Erro ao buscar dados de ${url}: ${response.statusText}`);
+                                }
+                                return response.json();
+                            })
+                    )
+                );
+                setDataOrganizador(result1.data)
+
+                // Aguarda todas as Promises usando Promise.all
+                const results = await Promise.all(fetchPromises);
+
+                // Armazena os resultados no estado
+                setData(results);
+            } catch (err) {
+                // Armazena o erro no estado
+                setErr(err.message);
+            } finally {
+                // Finaliza o carregamento
+                setFirstLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+
+    if (firstLoading) {
+        return (
+            <>
+                <TelaLoading />
+            </>
+        )
+    }
     //
     return (
         <>
@@ -32,17 +128,69 @@ export default function Home() {
                     </div>
 
                     <div className=" w-[90%]">
-                        <div className="pt-10">
+                        <div className="pt-10" onClick={() => console.log(dataOrganizador)}>
                             <h1 className="font-semibold text-slate-950 text-[30px] lg:text-[35px]"><span></span>CERTIFICADOS</h1>
                         </div>
                         <div>
                         </div>
                         <div className=" space-y-5">
-                            <h1 className="text-[#3E4095] hover:text-[#505191]">Você ainda não possui certificados disponíveis.</h1>
-                            <p className="text-[#54595f] text-justify">
-                                Seus certificados ainda não estão prontos. Mas não se preocupe! Estamos trabalhando para disponibilizá-los o quanto antes!
-                                <span className='text-gray-950 font-bold'> Caso possua alguma dúvida, fique a vontade para entrar em contato com a equipe COEPS.</span>
-                            </p>
+                            {
+                                !dataOrganizador ?
+                                    (
+                                        <div className=''>
+                                            <h1 className="text-[#3E4095] hover:text-[#505191]">Você ainda não possui certificados disponíveis.</h1>
+                                            <p className="text-[#54595f] text-justify">
+                                                Seus certificados ainda não estão prontos. Mas não se preocupe! Estamos trabalhando para disponibilizá-los o quanto antes!
+                                                <span className='text-gray-950 font-bold'> Caso possua alguma dúvida, fique a vontade para entrar em contato com a equipe COEPS.</span>
+                                            </p>
+                                        </div>
+                                    )
+                                    : ""
+                            }
+                            {
+                                dataOrganizador ?
+                                    (
+                                        <>
+                                            <div>
+                                                <h1 className='font-semibold text-slate-950 text-[20px] lg:text-[20px]'>🗂️ CERTIFICADOS ORGANIZAÇÃO</h1>
+                                            </div>
+                                            <div className='space-y-4'>
+                                                {dataOrganizador.map((value) => {
+                                                    return (
+                                                        <div key={value._id} onClick={() => console.log(value)} className=''>
+                                                            <div className="bg-white shadow-md w-full md:w-[55%] lg:w-[55%] rounded-xl p-5 border-t-2"
+                                                                style={{ borderTopColor: value.isOpen ? "#526eff" : "#ff5d52" }}
+                                                            >
+                                                                <div>
+                                                                    <p className='text-red-600 font-bold'>{value.type.toLocaleUpperCase()}</p>
+                                                                </div>
+                                                                <div className='text-gray-600'>
+                                                                    <p>
+                                                                        {
+                                                                            value.isOpen ?
+                                                                                (
+                                                                                    <p className='text-blue-950 cursor-pointer'
+                                                                                        onClick={()=> baixarCertificadoOrganizador(value._id)}
+                                                                                    >Clique para baixar seu certificado.</p>
+                                                                                )
+                                                                                :
+                                                                                (
+                                                                                    <p>
+                                                                                        Ainda não disponível.
+                                                                                    </p>
+                                                                                )
+                                                                        }
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </>
+                                    )
+                                    : ""
+                            }
                         </div>
                     </div>
 
@@ -52,4 +200,9 @@ export default function Home() {
         </>
 
     );
+
 }
+/*
+
+
+*/
---
+++ app/api/get/certificadoOrganizador/[_id]/route.js
@@ -0,0 +1,57 @@
+// pages/api/download.js
+import { ObjectId, GridFSBucket } from 'mongodb';
+import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
+import { connectToDatabase } from '@/app/lib/mongodb';
+
+export const dynamic = 'force-dynamic'
+
+export const GET = withApiAuthRequired(async function GET(request, { params }) {
+  try {
+    const { user } = await getSession();
+    const idComponente = params["_id"];
+
+    console.log(idComponente)
+
+    if (!ObjectId.isValid(idComponente)) {
+      return new Response(JSON.stringify({ message: "Não foi possível encontrar o arquivo. Recarregue a página e tente novamente. Caso o erro persista, entre em contato com a equipe COEPS." }), { status: 400, headers: { 'Content-Type': 'application/json' } });
+    }
+
+    const { db } = await connectToDatabase();
+
+    const data = await db.collection('organizadores').find({
+      _id: new ObjectId(idComponente)
+    }).toArray()
+
+    if (data.length != 1) {
+      return new Response(JSON.stringify({ message: "Não foi possível encontrar seu certificado. [ERROR: length != 1]." }), { status: 404, headers: { 'Content-Type': 'application/json' } });
+    }
+
+    const fileId = data[0].certificateId
+
+    const bucket = new GridFSBucket(db, { bucketName: 'organizadores' });
+
+    // Verifique se o arquivo pertence ao usuário
+    const file = await db.collection('organizadores.files').findOne({ _id: new ObjectId(fileId) });
+
+    if (!file) {
+      console.log(fileId)
+      return new Response(JSON.stringify({ message: "Arquivo não encontrado ou você não tem permissão para baixá-lo." }), { status: 404, headers: { 'Content-Type': 'application/json' } });
+    }
+
+    // Criar o fluxo de leitura do arquivo a partir do GridFS
+    const downloadStream = bucket.openDownloadStream(new ObjectId(fileId));
+
+    // Definir os cabeçalhos da resposta para o download
+    const headers = {
+      'Content-Type': file.contentType || 'application/octet-stream',
+      'Content-Disposition': `attachment; filename="${encodeURIComponent(file.filename)}"`
+    };
+
+    // Criar a resposta com o stream e os cabeçalhos
+    return new Response(downloadStream, { headers, status: 200 });
+
+  } catch (error) {
+    console.log(error.message);
+    return new Response(JSON.stringify({ message: error.message || "Ocorreu um erro desconhecido. Recarregue a página e tente novamente. Caso o erro persista, entre em contato com a equipe COEPS." }), { status: 500, headers: { 'Content-Type': 'application/json' } });
+  }
+});
