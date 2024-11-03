1f24ace0bded5daa80a86824384622fe45ae3cd0
Mateus <mateus2.0@icloud.com>
2024-11-03 20:06:12 -0300

    criação inicial de /certificados

---
+++ app/(unauth)/certificados/page.js
@@ -0,0 +1,15 @@
+'use client'
+//
+//
+export default function Anais() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-[#3E4095] flex justify-center content-center align-middle items-center ">
+        <div className="flex-1 min-h-screen flex justify-center content-center align-middle items-center">
+          <p className="text-white text-[40px] font-extrabold">EM BREVE!</p>
+        </div>
+      </div>
+    </>
+  )
+}
